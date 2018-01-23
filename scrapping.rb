1dcd69536a79ef001c45585b549f43a24eb61b47
D.Coloma <dnl.coloma@gmail.coma>
2018-01-23 16:51:05 +0100

    done

--- scrapping.rb
+++ scrapping.rb
@@ -5,18 +5,20 @@ require 'google_drive'
 require 'json'
 require 'pp'
 require 'csv'
+require 'pry'
+
 
 #récupère l'adresse email à partir de l'url d'une mairie
 def get_the_email_of_a_townhal_from_its_webpage(url)
 
     page = Nokogiri::HTML(open(url))
-
-    e_mails = page.css("tr > td.style27 > p.Style22") #recherche par CSS la section Adresse email
+    #recherche par CSS la section Adresse email
+    e_mails = page.css("tr > td.style27 > p.Style22")
     e_mails.each{|adr|
-
         #parmis les infos collecter, recherche les données contenant le caractère @ pour déterminer si la donnée = email
         if adr.text.include? "@"
-            return adr.text
+            #binding.pry
+            return adr.text[1...adr.text.length]# .strip pour effacer les éventuels espace avant et après le string
         end
     }
 end
@@ -25,55 +27,46 @@ end
 def get_all_the_urls_of_val_de_marne_townhalls(url)
     page = Nokogiri::HTML(open(url))
     list_url={}
-    website = page.xpath('//p/a')   #Recherche tous le code html des villes
-    #/tr[3]/td/table/tbody/tr/td[1]/table[2]/tbody/tr[3]/td/table/tbody/tr/td[1]/p[2]/a       VDM
-    #/tr[3]/td/table/tbody/tr/td[2]/p[2]/object/table/tbody/tr[2]/td/table/tbody/tr/td[1]/p/a[1] VDO
+    #Recherche tous le code html des villes
+    website = page.xpath('//p/a')
+
     website.each do |node|
-        #infos = []
-        url = "http://annuaire-des-mairies.com"+node['href'][1...node['href'].length]       #compléter l'URL de chaque mairie
-        #infos <<  url
-        #infos << get_the_email_of_a_townhal_from_its_webpage(url)
-        list_url[node.text]=get_the_email_of_a_townhal_from_its_webpage(url)#infos       #Hash{ Ville => [URL_mairie, email_mairie]}
+        #compléter l'URL de chaque mairie
+        url = "http://annuaire-des-mairies.com"+node['href'][1...node['href'].length]
+
+        #ajout dans un hash : Ville => email
+        list_url[node.text]=get_the_email_of_a_townhal_from_its_webpage(url)
 	end
     list_url
 end
 
 def put_in_spreadsheet(worksheet,get_hash)
     i = 1
+    # Pour chaque key dans mon hash
     get_hash.each do |key,value|
+        #affecter les keys dans chaque ligne dans la première colonne
         worksheet[i, 1] = key
+        #affecter les keys dans chaque ligne dans la deuxième colonne
         worksheet[i, 2] = value
         i += 1
     end
     worksheet.save
 end
 
-def put_in_json(get_hash)
-    File.open("email_mairie.json","w") do |f|
-        f.write(get_hash.to_json)
-    end
-end
+module Scrap
+    #ouverture de session googledrive avec les clés
+    SESSION = GoogleDrive::Session.from_config("config.json")
 
-def put_in_csv(get_hash)
-    CSV.open("email_mairie.csv", "wb") do |csv|
-        get_hash.each do |key,value|
-            csv << [key,value]
-        end
+    #Ouverture du fichier correspondant à la clé 1rgw...., worksheet 2
+    SHEET = SESSION.spreadsheet_by_key("1OfQxnGoIOkB05aLhEVapIO68qU7LYpls4cEr3EUcRHo")
+    #sheet.add_worksheet("Route de la mairie")
+    WS= SHEET.worksheets[0]
 
-    end
-end
-
-#ouverture de session googledrive avec les clés
-session = GoogleDrive::Session.from_config("config.json")
-
-#Ouverture du fichier correspondant à la clé 1rgw...., worksheet 2
-sheet = session.spreadsheet_by_key("1OfQxnGoIOkB05aLhEVapIO68qU7LYpls4cEr3EUcRHo")
-#sheet.add_worksheet("Route de la mairie")
-ws = sheet.worksheets[0]
 
 URL_VAL_DE_MARNE= "https://www.annuaire-des-mairies.com/val-de-marne.html"
 
-puts get_all_the_urls_of_val_de_marne_townhalls(URL_VAL_DE_MARNE)
-put_in_spreadsheet(ws,get_all_the_urls_of_val_de_marne_townhalls(URL_VAL_DE_MARNE))
+get_all_the_urls_of_val_de_marne_townhalls(URL_VAL_DE_MARNE)
+put_in_spreadsheet(WS,get_all_the_urls_of_val_de_marne_townhalls(URL_VAL_DE_MARNE))
 #put_in_json(get_all_the_urls_of_val_de_marne_townhalls(URL_VAL_DE_MARNE))
 #put_in_csv(get_all_the_urls_of_val_de_marne_townhalls(URL_VAL_DE_MARNE))
+end
--- send_emails.rb
+++ send_emails.rb
@@ -1,5 +1,12 @@
 require 'gmail'
 require 'dotenv'
+#require './scrapping'
+
+#$LOAD_PATH << File.dirname("/Users/danielcoloma/Desktop/THP/RepoGit/send_mail/scrapping.rb")
+#print $LOAD_PATH
+
+
+#include Scrap
 =begin
 class Object
   def to_imap_date
@@ -10,22 +17,38 @@ end
 =end
 Dotenv.load
 
-
-def send_email_to_line
-    gmail = Gmail.connect(ENV['email'],ENV['pass']) do |gmail|
-        puts gmail.inbox.count(:unread)
+#En paramètre : nom de la ville, @email
+def send_email_to_line(town,mail)
+    Gmail.connect(ENV['email'],ENV['pass']) do |gmail|
+        #puts gmail.inbox.count(:unread)
         gmail.deliver do
-            to "jeanmichel.lehacker@gmail.com"
-            subject "Mail Test from terminal "
-            body "Salut Jean Mich"
-        end
+            to mail
+            subject "Formation gratuite : The Hacking Project"
+            html_part do
+                content_type 'text/html; charset=UTF-8'
+                body "Bonjour,
+                Je m'appelle Daniel, je suis élève à une formation de code gratuite, ouverte à tous, sans restriction géographique, ni restriction de niveau. La formation s'appelle <a href='http://thehackingproject.org/'>The Hacking Project</a>. Nous apprenons l'informatique via la méthode du peer-learning : nous faisons des projets concrets qui nous sont assignés tous les jours, sur lesquels nous planchons en petites équipes autonomes. Le projet du jour est d'envoyer des emails à nos élus locaux pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation gratuite.
+
+                Nous vous contactons pour vous parler du projet, et vous dire que vous pouvez ouvrir une cellule à #{town}, où vous pouvez former gratuitement 6 personnes (ou plus), qu'elles soient débutantes, ou confirmées. Le modèle d'éducation de The Hacking Project n'a pas de limite en terme de nombre de moussaillons (c'est comme cela que l'on appelle les élèves), donc nous serions ravis de travailler avec #{town}!
+
+                Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80"
+            end
+       end
     end
 end
 
-def go_through_all_the_lines
-
+def go_through_all_the_lines(ws)
+    # Itération sur chaque ligne non vide du spreadsheet (grâce à la méthode .num_rows)
+    (1..ws.num_rows).each do |row|
+        # Envoi de mail en passant en paramètre : celulle_ville, cellule_adresse
+        send_email_to_line(ws[row, 1],ws[row, 2])
+        sleep(3)
+    end
 end
 
 def get_the_email_html
 
 end
+
+
+go_through_all_the_lines(Scrap::WS)
